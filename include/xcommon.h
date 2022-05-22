3104039809253b5df64ca414637587c1cdc7e05f
dimock <dimock1973@mail.ru>
2022-05-22 23:41:46 +0200

    mat generator off. see check ext. only

--- include/xcommon.h
+++ include/xcommon.h
@@ -212,7 +212,7 @@ namespace nst
 #endif
 
 #define SORT_MOVES_0_HIST
-#undef EXTEND_CHECK_SEE_ONLY
+#define EXTEND_CHECK_SEE_ONLY
 #define USE_LAZY_EVAL
 
 #ifndef PROCESS_MOVES_SEQ
@@ -244,7 +244,7 @@ namespace nst
 #undef VERIFY_LMR
 
 #undef SYNCHRONIZE_LAST_ITER
-#define GENERATE_MAT_CHECK_AFTER_HORIZON
+#undef GENERATE_MAT_CHECK_AFTER_HORIZON
 #undef AT_LEAST_ONE_MAT_MOVE
 
 static const SortValueType CaptureRecentlyBonus = 10;
